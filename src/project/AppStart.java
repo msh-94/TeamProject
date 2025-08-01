1bc61f25dc1bc7c734a9c231f53589bbd5ee39db
msh-94 <alstjdgh2003@naver.com>
2025-08-01 17:08:34 +0900

    로그인 기능 , 로그인 정보반환 기능 추가

--- src/project/AppStart.java
+++ src/project/AppStart.java
@@ -4,6 +4,6 @@ import project.controller.Member_HeadController;
 
 public class AppStart {// class start
     public static void main(String[] args) { // main start
-
+    // 푸시용
     }// main end
 }// class end
