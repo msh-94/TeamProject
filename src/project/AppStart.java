2280f8c7f715b714842473de3143ad1f43b837b1
root-kjs <root.kjs82@gmail.com>
2025-08-03 19:05:27 +0900

    (김진숙) 데스크탑(집) 개발환경 깃허브 연동 테스트3 --> 터미널 깃허브의 commit 계정 수정

--- src/project/AppStart.java
+++ src/project/AppStart.java
@@ -7,7 +7,7 @@ public class AppStart {
     public static void main(String[] args) {
 
         //ConnectDB.getInstance().connectDB(); // DB연동 테스트(김진숙)_250803
-        TotalView.getInstance().index(); // 본사 > 사용자단 > 메인공통화면(view)
+        TotalView.getInstance().index(); // 본사 사용자단 메인공통화면(view)
 
     }
 }//class end
