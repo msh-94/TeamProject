cf82168df78b5647927d7948428fe55de09d0f91
root-kjs <root.kjs82@gmail.com>
2025-08-06 18:40:50 +0900

    구독취소 작업중

--- src/project/view/UserView.java
+++ src/project/view/UserView.java
@@ -16,6 +16,8 @@ import java.util.Map;
 import static project.controller.CompanyController.currentCno;
 import static project.controller.Member_HeadController.currentMno;
 import static project.controller.PlanController.currentPno;
+import static project.model.dao.LogDao.toDay;
+import static project.view.TotalView.scan;
 
 public class UserView { // class start
     // 싱글톤
@@ -35,11 +37,11 @@ public class UserView { // class start
     /* (1) [본사]사용자단: 공통화면(로그인전/비회원/로그인 한 회원번호가 없는 경우_currentMno) ---------------------------------*/
     // 1.1.회원가입
     public void signUp(){
-        System.out.print("회원유형(1.일반회원  2.택시기사  3.사업자) :  "); int mCategory = TotalView.scan.nextInt();
-        System.out.print("아이디: "); String mId =TotalView.scan.next();
-        System.out.print("비밀번호: "); String mPwd = TotalView.scan.next();
-        System.out.print("이름: "); String mName = TotalView.scan.next();
-        System.out.print("전화번호: "); String mPhone = TotalView.scan.next();
+        System.out.print("회원유형(1.일반회원  2.택시기사  3.사업자) :  "); int mCategory = scan.nextInt();
+        System.out.print("아이디: "); String mId = scan.next();
+        System.out.print("비밀번호: "); String mPwd = scan.next();
+        System.out.print("이름: "); String mName = scan.next();
+        System.out.print("전화번호: "); String mPhone = scan.next();
 
         int result = mhc.signUp(mCategory,mId,mPwd,mName,mPhone);
         if(result==1){
@@ -53,8 +55,8 @@ public class UserView { // class start
 
     // 1.2.로그인
     public void logIn(){
-        System.out.print("아이디 : ");     String mId = TotalView.scan.next();
-        System.out.print("비밀번호 : ");    String mPwd = TotalView.scan.next();
+        System.out.print("아이디 : ");     String mId = scan.next();
+        System.out.print("비밀번호 : ");    String mPwd = scan.next();
         Member_HeadDto result = mhc.logIn(mId,mPwd);
         if (result.getmId() == null){
             System.out.println("[경고] 존재하지 않는 계정입니다.");
@@ -65,21 +67,10 @@ public class UserView { // class start
 
     // 1.3.구독신청
     public void subscribeRequest(){
-        System.out.println("\n3.구독신청\n");
+
         ArrayList<PlanDto> list = pc.planList();
         boolean bool = !currentPno.isEmpty(); // 중지플랜이 존재하면
-        int num = 1;
-        System.out.println("-----------------------------------------------------------------------------------------------------------");
-        for (int i = 0; i < list.size(); i++){
-            PlanDto dto = list.get(i);
-            if (bool){
-                if( currentPno.contains( dto.getPno() ) ){
-                    continue;
-                }// if end
-            }// if end
-            System.out.printf("%d. %s(%d달/%d원)\t",num++,dto.getpName(),dto.getpDate(),dto.getpMoney());
-        }// for end
-        System.out.println("\n-----------------------------------------------------------------------------------------------------------");
+
         if( currentMno != 0 ) { // 본사 로그인 이후, 구독신청 가능
             System.out.println("3.구독신청");
             /* 구독플랜조회 리스트 */ //planList();
@@ -91,17 +82,27 @@ public class UserView { // class start
             if( mLog != null  ){
                 for (PlanDto dto : planDtos) {
                     if (dto.getPno() == 1) continue;
+                    if (bool){
+                        if( currentPno.contains( dto.getPno() ) ){
+                            continue;
+                        }// if end
+                    }// if end
                     System.out.printf("  %d) %s (%d개월/%d원)\n", dto.getPno(), dto.getpName(), dto.getpDate(), dto.getpMoney());
                     continue;
                 }//for end
             }else {
                 for (PlanDto dto : planDtos) {
+                    if (bool){
+                        if( currentPno.contains( dto.getPno() ) ){
+                            continue;
+                        }// if end
+                    }// if end
                     System.out.printf("  %d) %s (%d개월/%d원)\n", dto.getPno(), dto.getpName(), dto.getpDate(), dto.getpMoney());
                 }//for e
             }
             System.out.println("-------------------------------");
             System.out.print("✔️ 선택 > ");
-            int choose = TotalView.scan.nextInt();
+            int choose = scan.nextInt();
 
             for (PlanDto dto : planDtos) {
                 if (dto.getPno() == choose) { selectPlan = dto; break; }
@@ -113,12 +114,12 @@ public class UserView { // class start
             String cName=""; String area=""; String service = "";
             if( mLog == null  ) {
                 System.out.print("\n - 사이트명 : ");
-                cName = TotalView.scan.next();
+                cName = scan.next();
                 System.out.print(" - 서비스지역 : ");
-                area = TotalView.scan.next();
-                TotalView.scan.nextLine();
+                area = scan.next();
+                scan.nextLine();
                 System.out.print(" - 서비스내용 : ");
-                service = TotalView.scan.nextLine();
+                service = scan.nextLine();
             }
             Map<String, Object> subscribeInfo = new HashMap<>();
             subscribeInfo.put("mno", currentMno);
@@ -133,7 +134,6 @@ public class UserView { // class start
         }else{ System.out.println("\n[안내] 로그인 이후, 구독신청 가능합니다.\n"); }// if end
     }//func end
 
-
     // 1.4.데모체험
     public void siteManasers(){
         for ( ; ; ){
@@ -142,7 +142,7 @@ public class UserView { // class start
                 System.out.println("< 데모체험 >  1.관리자단(ADMIN)         2. 사용자단(USER)         3. 본사바로가기");
                 System.out.println("------------------------------------------------------------------------------------------------------------");
                 System.out.print("선택 > ");
-                int choose = TotalView.scan.nextInt();
+                int choose = scan.nextInt();
                 if (choose == 1) {
                     System.out.println("관리자단");
                 } else if (choose == 2) {
@@ -154,7 +154,7 @@ public class UserView { // class start
                 }// if end
             }catch (InputMismatchException e) {
                 System.out.println("\n[경고] 입력타입 불일치! 숫자를 입력하세요." + e + "\n");
-                TotalView.scan = new Scanner(System.in);//입력객체 초기화
+                scan = new Scanner(System.in);//입력객체 초기화
             } catch (Exception e) {
                 System.out.println( "\n[오류] 개발팀 문의( root.kjs82@gmail.com )" + e + "\n" );
             }// catch end
@@ -176,9 +176,9 @@ public class UserView { // class start
     /* (2) [본사]사용자단: 회원(구독X/구독O) 로그인 화면 -------------------------------------------------------------------*/
     // 2.1.정보수정
     public void updateProfile(){
-        System.out.print("비밀번호 : ");    String mPwd = TotalView.scan.next();
-        System.out.print("수정할 비밀번호 : ");    String mPwd1 = TotalView.scan.next();
-        System.out.print("수정할 전화번호 : ");    String mPhone = TotalView.scan.next();
+        System.out.print("비밀번호 : ");    String mPwd = scan.next();
+        System.out.print("수정할 비밀번호 : ");    String mPwd1 = scan.next();
+        System.out.print("수정할 전화번호 : ");    String mPhone = scan.next();
         int result = mhc.updateProfile(mPwd,mPhone,mPwd1);
         if (result == 1){
             System.out.println("[안내] 정보수정이 완료되었습니다.");
@@ -208,7 +208,7 @@ public class UserView { // class start
                 System.out.printf("< %s >    1.관리자단(ADMIN)         2. 사용자단(USER)         3. 본사바로가기\n", result.getcName());
                 System.out.println("------------------------------------------------------------------------------------------------------------");
                 System.out.print("선택 > ");
-                int choose = TotalView.scan.nextInt();
+                int choose = scan.nextInt();
                 if (choose == 1) {
                     System.out.println("관리자단");
                 } else if (choose == 2) {
@@ -221,7 +221,7 @@ public class UserView { // class start
                 }// if end
             }catch (InputMismatchException e) {
                 System.out.println("\n[경고] 입력타입 불일치! 숫자를 입력하세요." + e + "\n");
-                TotalView.scan = new Scanner(System.in);//입력객체 초기화
+                scan = new Scanner(System.in);//입력객체 초기화
             } catch (Exception e) {
                 System.out.println( "\n[오류] 개발팀 문의( root.kjs82@gmail.com )" + e + "\n" );
             }// catch end
@@ -233,7 +233,7 @@ public class UserView { // class start
         System.out.println("6.구독현황\n");
         ArrayList<PlanDto> planDtos = pc.planList();
         LogDto result = lc.subscribeState( currentMno );
-        if( result != null ){
+        if( result != null  ){ // && result.getEndDate() == toDay
             PlanDto selectPlan = null; // 회원이 선택한 구독플랜 정보 1개 가져오기
             for (PlanDto dto : planDtos) {
                 if (dto.getPno() == result.getPno()) { selectPlan = dto; break; }
@@ -261,6 +261,14 @@ public class UserView { // class start
     // 2.8.구독취소
     public void subscribeCancle(){
         System.out.println("\n8.구독취소\n");
+
+        boolean result = lc.subscribeCancle( currentMno );
+        if( result ){
+            System.out.println("구독 취소되었습니다. ");
+        }else {
+            System.out.println("[오류] 관리자 문의(000-0000)");
+        }
+
     }//func end
 
 }// class end
