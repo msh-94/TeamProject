4fc77113cb395e9b656d13726781491dcf9249b9
l-yeong <yahn10@naver.com>
2025-08-08 11:23:22 +0900

    25.08.08 정은주

--- src/project/model/dao/CompanyDao.java
+++ src/project/model/dao/CompanyDao.java
@@ -1,15 +1,20 @@
 package project.model.dao; // 패키지명
 
 import project.model.dto.CompanyDto;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.ArrayList;
+
+
 
 public class CompanyDao extends Dao {// class start
     // 싱글톤
     private CompanyDao(){  }
     private static final CompanyDao instance = new CompanyDao();
     public static CompanyDao getInstance(){return instance; }
+
 
     // 로그인한 회원번호 일치하는 회사정보 반환 기능
     public CompanyDto siteManaser(int mno){
@@ -34,18 +39,39 @@ public class CompanyDao extends Dao {// class start
     public ArrayList<CompanyDto> taxiList(){
         ArrayList<CompanyDto> list = new ArrayList<>();
         try{
+            LocalDate today = LocalDate.now();
+
             String sql = "select * from company";
             PreparedStatement cs = conn.prepareStatement(sql);
             ResultSet rs = cs.executeQuery();
+
             while (rs.next()){
-                CompanyDto dto = new CompanyDto(
-                        rs.getInt("cno"),
-                        rs.getInt("mno"),
-                        rs.getString("cName"),
-                        rs.getString("area"),
-                        rs.getString("service")
+            int mno = rs.getInt("mno");
+            //종료일이 현재일보다 작으면 출력x
+            String sql1 = "select * from log where mno = ? And endDate < ?";
+            PreparedStatement logPs = conn.prepareStatement(sql1);
+            logPs.setInt(1,mno);
+            logPs.setDate(2,java.sql.Date.valueOf(today));
+            ResultSet logRs = logPs.executeQuery();
+
+            boolean result = logRs.next();// 구독중이면
+
+            logRs.close();
+            logPs.close();
+
+            if(result) continue;
+
+            CompanyDto dto = new CompanyDto(
+                    rs.getInt("cno"),
+                    rs.getInt("mno"),
+                    rs.getString("cName"),
+                    rs.getString("area"),
+                    rs.getString("service")
                 );list.add(dto);
             }
+
+            rs.close();
+            cs.close();
         } catch (Exception e) {
             System.out.println(e);
         }return list;
